83ddb76e578647ea77207b301e800af88797f38e
Valentin Novarino <87829@sistemas.frc.utn.edu.ar>
2025-11-10 12:40:24 -0300

    Ahora creo que funciona unu

--- src/main/java/ar/edu/utn/frc/backend/grupo114/Tarifas/Repository/TarifaRepository.java
+++ src/main/java/ar/edu/utn/frc/backend/grupo114/Tarifas/Repository/TarifaRepository.java
@@ -1,18 +1,11 @@
 package ar.edu.utn.frc.backend.grupo114.Tarifas.Repository;
 
-// Import del modelo desde su paquete correcto
 import ar.edu.utn.frc.backend.grupo114.Model.Tarifa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
-import java.util.Optional;
-
 @Repository
 public interface TarifaRepository extends JpaRepository<Tarifa, Long> {
 
-    // Ejemplo: Buscar una tarifa activa por nombre y fecha
-    Optional<Tarifa> findByNombreAndActivaIsTrueAndFechaInicioBeforeAndFechaFinAfter(
-            String nombre, LocalDate fecha
-    );
+
 }
